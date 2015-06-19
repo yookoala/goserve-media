fd142ee78b6124c508edd2c4a03de624b2bc465d
Koala Yeung <koalay@gmail.com>
2015-06-19 20:42:31 +0800

    Initial code to create a new custom server
    Want to make the listing better. Still working.

--- server/server.go
+++ server/server.go
@@ -1,9 +1,36 @@
 package server
 
 import (
+	"log"
 	"net/http"
+	"path"
+	"strings"
 )
 
-func New(dir string) http.Handler {
-	return http.FileServer(http.Dir(dir))
+// New creates new media server as http.Handler
+func New(dir string) *Server {
+	d := http.Dir(dir)
+	return &Server{
+		root: &d,
+	}
+}
+
+// Server implements http.Handler
+type Server struct {
+	root *http.Dir
+}
+
+// ServeHTTP implements http.Handler method
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	upath := r.URL.Path
+	if !strings.HasPrefix(upath, "/") {
+		upath = "/" + upath
+		r.URL.Path = upath
+	}
+
+	log.Printf("access path: %s", r.URL.Path)
+	log.Printf("access upath: %s", upath)
+	log.Printf("access: %s", path.Join(string(*s.root), path.Clean(upath)))
+	http.ServeFile(w, r, path.Join(string(*s.root), path.Clean(upath)))
 }
